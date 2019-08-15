e7dae582b9ba1a52244f56648b319095128b6822
chasinfo <chasinfo@gmail.com>
2019-08-14 23:34:42 -0300

    Atualizando o estudo sobre react

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import HelloWorld from './HelloWorld';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-     <HelloWorld txt='Mudando o texto do hello world!!!' />, 
-     document.getElementById('root')
-);
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
