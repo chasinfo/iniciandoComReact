2c44be390097a673665698603cc2fc938fba196d
chasinfo <chasinfo@gmail.com>
2019-09-04 22:59:33 -0300

    Adicionando arquivo Hello

---
+++ my-app/src/Hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hello extends Component
+{
+    render()
+    {
+        return (
+            <h1>Olá pessoal</h1>
+        );
+    }
+}
+
+export default Hello;
